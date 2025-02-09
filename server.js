9cfa1389e81aca97517df2aaef32c3e95c89f801
Vigneshkalvium <vipvignesh.dr.2244@gmail.com>
2025-02-09 10:05:40 +0530

    connection sTatus in home page

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 require('dotenv').config(); 
-
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const dotenv =require("dotenv");
 const connectDatabase = require('./database');
-
-app.get('/ping', (req, res) => {
-    res.send('Pong!');
-});
+dotenv.config();
 
 connectDatabase();
+app.get('/', (req, res) => {
+    const dbStatus = mongoose.connection.readyState === 1? "Database Connected Successfully" : "Database Connection Failed";
+    res.json({ status: dbStatus});
+});
+
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
